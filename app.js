923e576c74b2dc0eeb5877088f15b5365cd68832
kmc369 <keishacoleman710@gmail.com>
2023-07-14 20:56:47 -0400

    Building the structure

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
-window.addEventListener("DOMContentLoaded", ()=>{
-  
-})
+window.addEventListener("DOMContentLoaded" , ()=>{
+
+  //lets build the DOM
+  const leftTop = document.createElement("div");
+  const topMiddle = document.createElement("div");
+  const RightTop = document.createElement("div");
+
+  const topDiv = document.createElement("div");
+  topDiv.appendChild(leftTop);
+  topDiv.appendChild(topMiddle);
+  topDiv.appendChild(RightTop);
+
+  document.body.appendChild(topDiv);
+
+
+});
